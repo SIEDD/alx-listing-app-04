6267f9575381feb12db3d562c046c842f838f8a1
siedd <saidmwacharo8@gmail.com>
2025-02-17 10:53:45 +0300

    property

--- components/property/PropertyDetail.tsx
+++ components/property/PropertyDetail.tsx
@@ -1,1 +1,24 @@
+import React from "react";
 
+interface Property {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+}
+
+interface PropertyDetailProps {
+  property: Property;
+}
+
+const PropertyDetail: React.FC<PropertyDetailProps> = ({ property }) => {
+  return (
+    <div>
+      <h1>{property.name}</h1>
+      <p>{property.description}</p>
+      <p>Price: ${property.price}</p>
+    </div>
+  );
+};
+
+export default PropertyDetail;
